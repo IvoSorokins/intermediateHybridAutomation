9e486e15d4bb0f9fb17129fae7ac87b8f9a76321
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2023-11-28 17:19:10 +0200

    Small changes

--- src/main/java/utils/context.java
+++ src/main/java/utils/context.java
@@ -5,6 +5,10 @@ import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import io.appium.java_client.remote.SupportsContextSwitching;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 
 public class context {
 
@@ -24,6 +28,22 @@ public class context {
     }
     public void switchToWebView(String contextName) {
         ((SupportsContextSwitching) driver).context(contextName);
+    }
+
+    public String getSecondWebViewContext(){
+        // Get a handle to the open contexts
+        Set<String> contextNames = ((SupportsContextSwitching) driver).getContextHandles();
+
+        // Convert the Set to a List
+        List<String> contextList = new ArrayList<>(contextNames);
+
+        // Check if there is a second context
+        if (contextList.size() < 2) {
+            throw new IllegalStateException("No second WebView context found");
+        }
+
+        // Return the second context
+        return contextList.get(1);
     }
 
 
--- src/main/java/utils/shadowDomHelper.java
+++ src/main/java/utils/shadowDomHelper.java
@@ -6,8 +6,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
+
+import static utils.testProperties.waitTimeInSeconds;
 
 public class shadowDomHelper {
 
@@ -21,6 +26,7 @@ public class shadowDomHelper {
 
     // Code From : https://www.headspin.io/blog/working-with-web-components-shadow-dom
     public WebElement getNestedShadowRootElement(List<String> hostTagNames, String selector) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(waitTimeInSeconds));
         WebElement shadowHost = null;
         WebElement shadowElement = null;
         JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
@@ -40,6 +46,14 @@ public class shadowDomHelper {
         String jsScript = "return arguments[0].shadowRoot.querySelector('" + selector + "');";
         shadowElement = (WebElement) jsExecutor.executeScript(jsScript, shadowHost);
 
+        return shadowElement;
+    }
+    public WebElement getShadowRootElement(String hostSelector, String shadowSelector) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(waitTimeInSeconds));
+        WebElement shadowHost = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(hostSelector)));
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        WebElement shadowRoot = (WebElement) jsExecutor.executeScript("return arguments[0].shadowRoot", shadowHost);
+        WebElement shadowElement = wait.until(ExpectedConditions.visibilityOf((WebElement) jsExecutor.executeScript("return arguments[0].querySelector(arguments[1])", shadowRoot, shadowSelector)));
         return shadowElement;
     }
 }
