5b2b94b78d0a6a782a3664175bc642f01a87c0ea
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2023-11-28 20:05:12 +0200

    Fixed ionicAccountScreen

--- src/main/java/screens/ionicAccountScreen.java
+++ src/main/java/screens/ionicAccountScreen.java
@@ -3,40 +3,44 @@ package screens;
 // Appium , TestNG imports
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+
 import org.openqa.selenium.support.PageFactory;
 
 // utils imports
 import utils.context;
 import utils.interactions;
 
+import static org.testng.AssertJUnit.assertEquals;
+
 
 public class ionicAccountScreen {
+
     private final AppiumDriver driver;
-    public interactions Interactions;
-    public context Context;
-
-    @FindBy(xpath="//h1[@id=\"modal-header\"]")
-    private WebElement twitterHeader;
-
-    @FindBy(xpath="//div[@id=\"repository-container-header\"]")
-    private WebElement githubHeader;
-
-    @FindBy(xpath="//page-speaker-detail/ion-content")
-    private WebElement speakerDetailContent;
-
 
     public ionicAccountScreen(AppiumDriver driver) {
         this.driver = driver;
         PageFactory.initElements(new AppiumFieldDecorator(this.driver), this);
-        Interactions = new interactions(driver);
-        Context = new context(driver);
+
     }
 
-    public void verifyWebpageIsDisplayed(){
-        String secondWebViewContext = Context.getSecondWebViewContext();
-        System.out.println("Second webview context: " + secondWebViewContext);
-        Context.switchToWebView(secondWebViewContext);
+    public void switchToNewWindow(){
+        for (String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);
+        }
     }
+    public String getCurrentURL() {
+        return driver.getCurrentUrl();
+    }
+    public void verifyUrl(String expectedUrl){
+        // Switch to the new window
+        switchToNewWindow();
+
+        // Get the current URL
+        String currentUrl = getCurrentURL();
+        System.out.print("Current url: "+ currentUrl +"\n Expected url: "+ expectedUrl);
+
+        // Assert that the current URL equals the expected URL
+        assertEquals("URL is not expected", expectedUrl, currentUrl);
+    }
+
 }
