3f67f107490024c9f4e08a6b1fcea18b9d275925
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2023-11-30 14:40:12 +0200

    Added description to eventsData

--- src/main/java/dataProviders/eventsData.java
+++ src/main/java/dataProviders/eventsData.java
@@ -1,31 +1,32 @@
 package dataProviders;
 
+import dataObjects.event;
 import org.testng.annotations.DataProvider;
 
 public class eventsData {
         @DataProvider(name = "eventProvider")
         public Object[][] provideData() {
             return new Object[][]{
-                    {"Breakfast"},
-                    {"Getting Started with Ionic"},
-                    {"Ionic Tooling"},
-                    {"University of Ionic"},
-                    {"Migrating to Ionic"},
-                    {"What's New in Angular"},
-                    {"The Evolution of Ionicons"},
-                    {"Ionic Pro"},
-                    {"Ionic Workshop"},
-                    {"Community Interaction"},
-                    {"Navigation in Ionic"},
-                    {"Lunch"},
-                    {"Ionic in the Enterprise"},
-                    {"Ionic Worldwide"},
-                    {"The Ionic Package"},
-                    {"Push Notifications in Ionic"},
-                    {"Ionic Documentation"},
-                    {"UX in Ionic"},
-                    {"Angular Directives in Ionic"},
-                    {"Mobile States"}
+                    {new event("Breakfast","8:00 am — 9:00 am: Dining Hall")},
+                    {new event("Getting Started with Ionic","9:30 am — 9:45 am: Hall 2")},
+                    {new event("Ionic Tooling","9:45 am — 10:00 am: Executive Ballroom")},
+                    {new event("University of Ionic","9:15 am — 9:30 am: Hall 3")},
+                    {new event("Migrating to Ionic","10:00 am — 10:15 am: Hall 1")},
+                    {new event("What's New in Angular","10:15 am — 10:30 am: Hall 3")},
+                    {new event("The Evolution of Ionicons","10:15 am — 10:30 am: Hall 2")},
+                    {new event("Ionic Pro","10:45 am — 11:00 am: Grand Ballroom A")},
+                    {new event("Ionic Workshop","11:00 am — 11:45 am: Hall 1")},
+                    {new event("Community Interaction","11:30 am — 11:50 am: Hall 3")},
+                    {new event("Navigation in Ionic","11:30 am — 12:00 pm: Grand Ballroom A")},
+                    {new event("Lunch","12:00 pm — 1:00 pm: Dining Hall")},
+                    {new event("Ionic in the Enterprise","1:00 pm — 1:15 pm: Hall 1")},
+                    {new event("Ionic Worldwide","1:15 pm — 1:30 pm: Hall 1")},
+                    {new event("The Ionic Package","1:30 pm — 2:00 pm: Grand Ballroom B")},
+                    {new event("Push Notifications in Ionic","2:00 pm — 2:30 pm: Hall 2")},
+                    {new event("Ionic Documentation","2:30 pm — 2:45 pm: Grand Ballroom B")},
+                    {new event("UX in Ionic","2:45 pm — 3:00 pm: Hall 3")},
+                    {new event("Angular Directives in Ionic","3:00 pm — 3:30 pm: Hall 1")},
+                    {new event("Mobile States","3:30 pm — 3:45 pm: Hall 2")}
         };
     }
 }
