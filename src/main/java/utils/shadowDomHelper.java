4a7f0cc496b8e0139c0047fb20a6c7cc354ff9f9
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2023-11-30 16:57:44 +0200

    fixed ShadowDomHelper

--- src/main/java/utils/shadowDomHelper.java
+++ src/main/java/utils/shadowDomHelper.java
@@ -2,17 +2,9 @@ package utils;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-import java.util.List;
 
-import static utils.testProperties.waitTimeInSeconds;
 
 public class shadowDomHelper {
 
@@ -23,37 +15,9 @@ public class shadowDomHelper {
         PageFactory.initElements(new AppiumFieldDecorator(this.driver), this);
 
     }
-
-    // Code From : https://www.headspin.io/blog/working-with-web-components-shadow-dom
-    public WebElement getNestedShadowRootElement(List<String> hostTagNames, String selector) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(waitTimeInSeconds));
-        WebElement shadowHost = null;
-        WebElement shadowElement = null;
-        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
-
-        for (String hostTagName : hostTagNames) {
-            if (shadowHost == null) {
-                // Find the first host element of the Shadow DOM
-                shadowHost = driver.findElement(By.cssSelector(hostTagName));
-            } else {
-                // Find the next host element inside the Shadow DOM of the previous host element
-                String jsScript = "return arguments[0].shadowRoot.querySelector('" + hostTagName + "');";
-                shadowHost = (WebElement) jsExecutor.executeScript(jsScript, shadowHost);
-            }
-        }
-
-        // Find the final element inside the Shadow DOM of the last host element
-        String jsScript = "return arguments[0].shadowRoot.querySelector('" + selector + "');";
-        shadowElement = (WebElement) jsExecutor.executeScript(jsScript, shadowHost);
-
-        return shadowElement;
-    }
-    public WebElement getShadowRootElement(String hostSelector, String shadowSelector) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(waitTimeInSeconds));
-        WebElement shadowHost = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(hostSelector)));
-        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
-        WebElement shadowRoot = (WebElement) jsExecutor.executeScript("return arguments[0].shadowRoot", shadowHost);
-        WebElement shadowElement = wait.until(ExpectedConditions.visibilityOf((WebElement) jsExecutor.executeScript("return arguments[0].querySelector(arguments[1])", shadowRoot, shadowSelector)));
-        return shadowElement;
+    private String getMarkHelper(String hostSelector,String childSelector){
+        String mark = "return document.querySelector(\""+hostSelector+"\").shadowRoot.querySelector(\""+childSelector+"\")";
+                            //document.querySelector("ion-item-group:nth-child(1) > ion-item-sliding > ion-item-options ion-item-option").shadowRoot.querySelector("button");
+        return mark;
     }
 }
